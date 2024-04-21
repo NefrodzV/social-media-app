6af123989d2101977f906eaef9cf057592f3ef87
Neftaly <neftalyrv23@gmail.com>
2024-04-21 19:41:16 -0400

    made style for loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,3 +1,12 @@
-export default function Loader({height, width}) {
-    return <div></div>
+export default function Loader({ height, width, borderWidth }) {
+    return (
+        <div
+            className="loader"
+            style={{
+                width,
+                height,
+                borderWidth,
+            }}
+        ></div>
+    )
 }
