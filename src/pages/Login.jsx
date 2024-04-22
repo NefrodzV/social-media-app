dc354fe1e9247f81fde832e62c37e29a03475b62
Neftaly <neftalyrv23@gmail.com>
2024-04-22 17:24:49 -0400

    Made the login skeleton elements

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,29 @@
+import { Input, Group } from '../components/index'
+
 export default function Login() {
-    return <div>Login</div>
+    function loginHandler(e) {
+        e.preventDefault()
+    }
+    return (
+        <div>
+            <form onSubmit={loginHandler} noValidate={true}>
+                <h1>Login</h1>
+                <Group>
+                    <Input
+                        name={'email'}
+                        type={'email'}
+                        placeholder={'Email'}
+                    />
+                </Group>
+                <Group>
+                    <Input
+                        name={'password'}
+                        type={'password'}
+                        placeholder={'password'}
+                    />
+                </Group>
+                <button>Login</button>
+            </form>
+        </div>
+    )
 }
