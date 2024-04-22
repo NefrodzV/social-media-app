b23c0836771fa3c6de3e402cd5c62583bc97dd06
Neftaly <neftalyrv23@gmail.com>
2024-04-22 17:52:00 -0400

    made the logic to login to account

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
+import { STATUS } from '../constants'
 import { Input, Group } from '../components/index'
 
 export default function Login() {
+    const [status, setStatus] = useState(null)
+    const [data, setData] = useState(null)
+    async function login(data) {
+        try {
+            setStatus(STATUS.PENDING)
+            const response = await fetch(
+                'http://localhost:3000/session/login',
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data),
+                }
+            )
+            const json = await response.json()
+            if (!response.ok) {
+                setStatus(STATUS.ERROR)
+                return console.log(json)
+            }
+            setStatus(STATUS.SUCCESS)
+        } catch (e) {
+            setStatus(STATUS.ERROR)
+            throw new Error('Login action error: ' + e)
+        }
+    }
     function loginHandler(e) {
         e.preventDefault()
+        login(new FormData(e.target))
     }
     return (
         <div>
-            <form onSubmit={loginHandler} noValidate={true}>
+            <form onSubmit={loginHandler} noValidate>
                 <h1>Login</h1>
                 <Group>
                     <Input
@@ -22,7 +49,7 @@ export default function Login() {
                         placeholder={'password'}
                     />
                 </Group>
-                <button>Login</button>
+                <button disabled={status === 'pending'}>Login</button>
             </form>
         </div>
     )
