aaf0b6bc9a3c18daf136815ebe443272c74437b8
Neftaly <neftalyrv23@gmail.com>
2024-05-01 19:15:51 -0400

    Made values to json obvjects because booleans are saved as strings and cant apply checks for booleans saved

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -2,10 +2,10 @@ export default function useLocalStorage() {
     const localStorage = window.localStorage
 
     const set = (key, val) => {
-        localStorage.setItem(key, val)
+        localStorage.setItem(key, JSON.stringify(val))
     }
     const get = (key) => {
-        return localStorage.getItem(key)
+        return JSON.parse(localStorage.getItem(key))
     }
 
     const remove = (key) => {
