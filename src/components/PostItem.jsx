d1fbb4b7709bdd2e1275dd4c1a4d57a8b8783c7c
Neftaly <neftalyrv23@gmail.com>
2024-05-07 19:15:10 -0400

    Fixed how the list is implemented

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,16 +1,11 @@
 import PropTypes from 'prop-types'
 import CommentList from './CommentList'
-import CommentItem from './CommentItem'
 export default function PostItem({ post }) {
     return (
         <li data-id={post?._id}>
             <div>{post?.user?.firstName + ' ' + post?.user?.lastName}</div>
-            <div>{post.text}</div>
-            <CommentList postId={post?._id}>
-                {post.comments.map((comment) => (
-                    <CommentItem key={comment?._id} comment={comment} />
-                ))}
-            </CommentList>
+            <div>{post?.text}</div>
+            <CommentList postId={post?._id} comments={post?.comments} />
         </li>
     )
 }
