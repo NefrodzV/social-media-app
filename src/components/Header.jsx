56034681009a1be5d954fd6084de2ae2ea3fca42
Neftaly <neftalyrv23@gmail.com>
2024-05-09 12:11:44 -0400

    Added the navigation component here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import Navigation from './Navigation'
+
 export default function Header() {
     return (
         <header>
             <nav>Mobile nav</nav>
             <span>Logo</span>
+            <Navigation />
         </header>
     )
 }
