b3015662c7c452049259462d9fad8989e5128010
Neftaly <neftalyrv23@gmail.com>
2024-05-16 11:28:57 -0400

    Made function to showModal Dialogs

--- src/hooks/useDialog.jsx
+++ src/hooks/useDialog.jsx
@@ -1,8 +1,25 @@
 import { useContext } from 'react'
 import { DialogContext } from '../App'
+import { DIALOG_TYPE } from '../constants'
 export default function useDialog() {
-    const context = useContext(DialogContext)
+    const {
+        setDialog,
+        dialog,
+        DIALOG_TYPE: { MODAL },
+        closeDialog,
+    } = useContext(DialogContext)
 
-    if (!context) throw new Error('Dialog context provider not defined')
-    return context
+    function showModal(component) {
+        if (!component || component === undefined) {
+            throw new Error('showModal doesnt have a component is ' + component)
+        }
+        const dialog = {
+            type: MODAL,
+            component,
+        }
+
+        setDialog(dialog)
+    }
+    
+    return { setDialog, dialog, showModal, DIALOG_TYPE, closeDialog }
 }
