da56586f08da736a455d38037e73003628f96915
Neftaly <neftalyrv23@gmail.com>
2024-05-27 13:01:51 -0400

    made hook to get user with userid

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { STATUS } from '../constants'
+
+export default function useUser(id) {
+    const { SUCCESS, ERROR, PENDING } = STATUS
+    const [status, setStatus] = useState(null)
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        async function getUser() {
+            try {
+                setStatus(PENDING)
+                const url = import.meta.env.VITE_API_URL
+                const response = await fetch(url + '/users/' + id, {
+                    mode: 'cors',
+                    credentials: 'include',
+                })
+
+                const json = await response.json()
+                if (!response.ok) {
+                    setStatus(ERROR)
+                    throw new Error('GET User with id error: ' + json)
+                }
+                setStatus(SUCCESS)
+                setUser(json.user)
+            } catch (e) {
+                setStatus(ERROR)
+                throw new Error('GET User with id error: ' + e)
+            }
+        }
+        if (id) getUser()
+    }, [ERROR, PENDING, SUCCESS, id])
+
+    return { user, status }
+}
