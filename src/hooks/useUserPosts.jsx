f3aa21895b686ef9d9ef73f3ae05c5e8bd8bd05a
Neftaly <neftalyrv23@gmail.com>
2024-05-27 13:01:36 -0400

    Made a hook to get the user posts with user id

---
+++ src/hooks/useUserPosts.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { STATUS } from '../constants'
+
+export default function useUserPosts(id) {
+    const { SUCCESS, PENDING, ERROR } = STATUS
+    const [status, setStatus] = useState(null)
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        async function getPostsWithUserId() {
+            try {
+                setStatus(PENDING)
+                const url = import.meta.env.VITE_API_URL
+                const response = await fetch(url + '/users/' + id + '/posts', {
+                    mode: 'cors',
+                    credentials: 'include',
+                })
+
+                const json = await response.json()
+                if (!response.ok) {
+                    setStatus(ERROR)
+                    throw new Error(
+                        'GET User posts with user id error: ' + json
+                    )
+                }
+
+                setStatus(SUCCESS)
+                setPosts(json.posts)
+            } catch (e) {
+                setStatus(ERROR)
+                throw new Error('GET User posts with user id error: ' + e)
+            }
+            if (id) getPostsWithUserId()
+        }
+    }, [ERROR, PENDING, SUCCESS, id])
+    return { posts, status }
+}
