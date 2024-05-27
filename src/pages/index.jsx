4fcad47992f2d637a223a61946458829b14b1432
Neftaly <neftalyrv23@gmail.com>
2024-05-27 13:14:54 -0400

    fixed typo in import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 export { default as HomePage } from './HomePage'
 export { default as LoginPage } from './LoginPage'
 export { default as SignupPage } from './SignupPage'
-export { default as ProfilePage } from './Profile'
+export { default as UserPage } from './UserPage'
 export { default as PostPage } from './PostPage'
