fd5e57e43fb3ac354701e29796198b0a1c0bfd8e
Neftaly <neftalyrv23@gmail.com>
2024-05-31 14:43:39 -0400

    Set logout function to async and passed some parameters for the login pages that are necessary

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -4,38 +4,60 @@ import { AuthContext } from '../contexts'
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [status, setStatus] = useState(null)
+    const [errors, setErrors] = useState(null)
     const { SUCCESS, PENDING, ERROR } = STATUS
+
     const login = async (data) => {
-        async function login(data) {
-            try {
-                setStatus(PENDING)
-                const response = await fetch(
-                    `${import.meta.env.VITE_API_URL}/session/login`,
-                    {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(data),
-                        mode: 'cors',
-                    }
-                )
-                const json = await response.json()
-                if (!response.ok) {
-                    setStatus(ERROR)
-                    setErrors(json.errors)
-                    return
+        try {
+            setStatus(PENDING)
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/session/login`,
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data),
+                    mode: 'cors',
                 }
-                // If the login response was successfull
-                setStatus(SUCCESS)
-                setUser(json.user)
-            } catch (e) {
+            )
+            const json = await response.json()
+            if (!response.ok) {
                 setStatus(ERROR)
-                throw new Error('Login action error: ' + e)
+                setErrors(json.errors)
+                return
             }
+            // If the login response was successfull
+            setStatus(SUCCESS)
+            setUser(json.user)
+            console.log(json)
+        } catch (e) {
+            setStatus(ERROR)
+            throw new Error('Login action error: ' + e)
         }
     }
-    function logout() {}
+    async function logout() {
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/session/logout`,
+                {
+                    method: 'POST',
+                    credentials: 'include',
+                }
+            )
+
+            if (!response.ok) {
+                const json = await response.json()
+                throw new Error(json)
+            }
+
+            setUser(null)
+        } catch (e) {
+            throw new Error('Error logging out: ' + e)
+        }
+    }
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider
+            value={{ user, status, errors, setUser, login, logout }}
+        >
             {children}
         </AuthContext.Provider>
     )
