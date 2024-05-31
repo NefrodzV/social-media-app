e723acc1aa7dfdb98c8b8549b5573da6357601aa
Neftaly <neftalyrv23@gmail.com>
2024-05-31 14:26:04 -0400

    Created new contexts for dialog and notifications

---
+++ src/contexts/index.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+export const AuthContext = createContext(null)
+export const NotificationContext = createContext(null)
+export const DialogContext = createContext(null)
