7d31bde04ade0bc7756ca4dce1342920e77a920e
Neftaly <neftalyrv23@gmail.com>
2024-05-31 18:45:11 -0400

    Fixed export of hooks and added the posts in the profile page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,44 @@
+import { Layout, ImageForm, PostList, PostItem, Loader } from '../components'
+import { useAuth, useAuthUser, useAuthUserPosts, useDialog } from '../hooks'
+import basicUserIcon from '../assets/example-user.jpg'
+export default function ProfilePage() {
+    const { user } = useAuth()
+    const { updateImage } = useAuthUser()
+    const { showModal } = useDialog()
+    const { posts } = useAuthUserPosts()
+    return (
+        <>
+            {user ? (
+                <Layout>
+                    <div>
+                        <div>
+                            <h2>{user?.fullname}</h2>
+                            <img
+                                src={user?.imgUrl ? user.imgUrl : basicUserIcon}
+                                alt="user profile picture"
+                                width={50}
+                                height={50}
+                            />
+                        </div>
+                        <button
+                            onClick={() => {
+                                showModal(
+                                    <ImageForm onSuccessHandler={updateImage} />
+                                )
+                            }}
+                        >
+                            Change image
+                        </button>
+                        <PostList>
+                            {posts.map((post) => (
+                                <PostItem key={post?._id} post={post} />
+                            ))}
+                        </PostList>
+                    </div>
+                </Layout>
+            ) : (
+                <Loader />
+            )}
+        </>
+    )
+}
