656e1868c72e3d096e981cd5ab3a6e03654a2e84
Neftaly <neftalyrv23@gmail.com>
2024-06-06 14:28:05 -0400

    Made the basic component for the user request list

---
+++ src/components/RequestList.jsx
@@ -0,0 +1,17 @@
+export default function RequestList() {
+    return (
+        <div>
+            <h1>Requests</h1>
+            <ul>
+                <li>
+                    <div>Recieved</div>
+                    <ul></ul>
+                </li>
+                <li>
+                    <div>Sent</div>
+                    <ul></ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
