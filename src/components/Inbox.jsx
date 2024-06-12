43105a4e730bd96d701e042f71f514028e3c7828
Neftaly <neftalyrv23@gmail.com>
2024-06-12 09:37:04 -0400

    Made a skeleton for inbox for request sent by auth user and sent from other users

---
+++ src/components/Inbox.jsx
@@ -0,0 +1,11 @@
+export default function Inbox() {
+    return (
+        <div>
+            <h1>Requests</h1>
+            <h2>Recieved</h2>
+            <ul></ul>
+            <h2>Sent</h2>
+            <ul></ul>
+        </div>
+    )
+}
