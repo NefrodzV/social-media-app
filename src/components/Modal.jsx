b46879c5002e6d5d76153621f56c9eed9e2f0118
Neftaly <neftalyrv23@gmail.com>
2024-06-12 10:32:21 -0400

    Modified modal to contain the close button by default

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useRef } from 'react'
-
+import { useDialog } from '../hooks'
 export default function Modal({ children }) {
     const dialogRef = useRef()
+    const { closeDialog } = useDialog()
     useEffect(() => {
         const dialog = dialogRef?.current
         dialog.showModal()
     }, [])
-    return <dialog ref={dialogRef}>{children}</dialog>
+    return (
+        <dialog ref={dialogRef}>
+            <button onClick={closeDialog}>close</button>
+            {children}
+        </dialog>
+    )
 }
