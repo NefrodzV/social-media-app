17519432b0a78ac73cbad39fa661a28f078813e8
Neftaly <neftalyrv23@gmail.com>
2024-06-14 16:02:22 -0400

    Update the page to use the user list

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,11 +1,13 @@
-import { Layout, PostList, PostItem, Loader, FollowerList } from '../components'
+import { Layout, PostList, PostItem, Loader, UserList } from '../components'
 import userIcon from '../assets/example-user.jpg'
 import { useLocation } from 'react-router-dom'
 import { useUser, useUserPosts } from '../hooks'
 export default function UserPage() {
     const location = useLocation()
     const { id } = location.state
+    // Update the api so the user returns their posts
     const { user } = useUser(id)
+    console.log(user)
     const { posts } = useUserPosts(id)
     return (
         <>
@@ -27,11 +29,9 @@ export default function UserPage() {
                             ))}
                         </PostList>
                     </div>
-                    <FollowerList
+                    <UserList
+                        title={`${user.fullname} followers`}
                         followers={user?.followers}
-                        enableAdd={true}
-                        enableDelete={false}
-                        enableEdit={false}
                     />
                 </Layout>
             ) : (
