853080dd8fde63106a6e0c8c8241a7a1e9b5a490
Neftaly <neftalyrv23@gmail.com>
2024-06-17 11:32:18 -0400

    Made it so that user can see comments only when they choose to and remove the comments when the user leaves the post

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,32 +4,36 @@ import { Link } from 'react-router-dom'
 import { useDialog } from '../hooks'
 import CreateCommentForm from './CreateCommentForm'
 import CommentList from './CommentList'
+import { useState } from 'react'
 export default function PostItem({ post }) {
     const {
         user: { firstName, lastName },
     } = post
     const { showModal } = useDialog()
 
+    const [isCommentsOpen, setIsCommentsOpen] = useState(false)
     function showCommentForm() {
         showModal(<CreateCommentForm postId={post?._id} />)
     }
     return (
-        <article className="post-item" data-id={post?._id}>
-            {/* <Link
-                to={`/${firstName + lastName}/posts/${post?._id}`}
-                state={post}
-            >
-                Go to post by {firstName + ' ' + lastName}
-            </Link> */}
+        <article
+            onMouseLeave={() => setIsCommentsOpen(false)}
+            className="post-item"
+            data-id={post?._id}
+        >
             <div>{post?.user?.firstName + ' ' + post?.user?.lastName}</div>
             <div>{post?.text}</div>
             <Group>
                 <button onClick={showCommentForm}>Comment</button>
                 <button>Like</button>
                 <button>Share</button>
-                <button>See comments</button>
+                {!isCommentsOpen && (
+                    <button onClick={() => setIsCommentsOpen(true)}>
+                        See comments
+                    </button>
+                )}
             </Group>
-            <CommentList postId={post?._id} />
+            {isCommentsOpen && <CommentList postId={post?._id} />}
         </article>
     )
 }
