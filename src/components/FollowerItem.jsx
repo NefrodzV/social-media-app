3eb89c90ebc08311346d7037e75296b3bc9a3d20
Neftaly <neftalyrv23@gmail.com>
2024-06-17 10:57:30 -0400

    Implemented user image

--- src/components/FollowerItem.jsx
+++ src/components/FollowerItem.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { useImages } from '../hooks'
 export default function FollowerItem({ follower, deleteFollower, isEditing }) {
     const { user } = follower
+    const { userSolidSvg } = useImages()
+    console.log(user)
+    console.log(user?.imgUrl)
     return (
         <article>
             <Link to={`/${user?.fullname}`} state={{ id: user?._id }}>
                 Go view follower profile
             </Link>
+            <img
+                src={user.imgUrl || userSolidSvg}
+                alt="user account image"
+                style={{
+                    width: 50,
+                }}
+            />
             {user?.fullname}
             {isEditing && (
                 <button onClick={deleteFollower?.bind('id', follower?._id)}>
