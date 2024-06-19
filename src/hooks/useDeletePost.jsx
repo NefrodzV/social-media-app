2073300df18e6a535a4dcc6f6cbbd3b269dfedd7
Neftaly <neftalyrv23@gmail.com>
2024-06-19 17:32:46 -0400

    Made that the dialog closes, show a toast if deletion is successfull or not, and implemented to removed the post in the state.

--- src/hooks/useDeletePost.jsx
+++ src/hooks/useDeletePost.jsx
@@ -1,25 +1,38 @@
 import { useState } from 'react'
 import { STATUS } from '../constants'
+import useNotification from './useNotification'
+import useAuthUser from './useAuthUser'
+import useDialog from './useDialog'
 export default function useDeletePost({ postId }) {
-    const { PENDING, ERROR, SUCCESS } = STATUS
-    const [status, setStatus] = useState(null)
-
+    // const { PENDING, ERROR, SUCCESS } = STATUS
+    // const [status, setStatus] = useState(null)
+    const { showToast } = useNotification()
+    const { removePost } = useAuthUser()
+    const { closeDialog } = useDialog()
     async function deletePost() {
         try {
-            setStatus(PENDING)
+            // setStatus(PENDING)
             const url = import.meta.env.VITE_API_URL
-            const response = await fetch(url + '/posts/' + postId, {
+            const response = await fetch(url + '/users/me/posts/' + postId, {
                 method: 'DELETE',
                 credentials: 'include',
                 mode: 'cors',
             })
-            if (!response.ok) return setStatus(ERROR)
-            setStatus(SUCCESS)
+            if (!response.ok) {
+                throw new Error('DELETE post error: ' + response.status)
+            }
+            console.log('post deleted')
+            closeDialog()
+            showToast('Post delete successfully')
+            removePost(postId)
+
+            // setStatus(SUCCESS)
         } catch (e) {
-            setStatus(ERROR)
-            throw new Error('DELETE post error:' + e)
+            // setStatus(ERROR)
+            showToast('Post couldnt be deleted. Please try again.')
+            console.error('DELETE post error:' + e)
         }
     }
 
-    return { deleteStatus: status, deletePost }
+    return { deletePost }
 }
