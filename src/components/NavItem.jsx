d85347cd435ca3ed3ea5c9f21ee39228c7bf15d0
Neftaly <neftalyrv23@gmail.com>
2024-06-28 13:01:55 -0400

    Changed the navItem logic

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,3 +1,28 @@
-export default function NavItem({ children }) {
-    return <li>{children}</li>
+import { Link } from 'react-router-dom'
+
+export default function NavItem({ text, icon, onClickHandler, navigateTo }) {
+    return (
+        <li className="nav-item">
+            {onClickHandler && (
+                <button onClick={onClickHandler}>
+                    <img
+                        className="icon"
+                        src={icon}
+                        alt="navigation item icon"
+                    />
+                    {text}
+                </button>
+            )}
+            {navigateTo && (
+                <Link to={navigateTo}>
+                    <img
+                        className="icon"
+                        src={icon}
+                        alt="navigation item icon"
+                    />
+                    {text}
+                </Link>
+            )}
+        </li>
+    )
 }
