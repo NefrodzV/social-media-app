0a173b3e1c03c5c680e4dcf2d1093c4bb61cae86
Neftaly <neftalyrv23@gmail.com>
2024-07-11 17:59:47 -0400

    Styled the modal close button and added a new space dimen

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useRef } from 'react'
-import { useDialog } from '../hooks'
+import { useDialog, useImages } from '../hooks'
+import style from '../stylesheets/Modal.module.css'
 export default function Modal({ children }) {
     const dialogRef = useRef()
     const { closeDialog } = useDialog()
+    const { xmarkSvg } = useImages()
     useEffect(() => {
         const dialog = dialogRef?.current
         dialog.showModal()
     }, [])
     return (
-        <dialog ref={dialogRef}>
-            <button onClick={closeDialog}>close</button>
+        <dialog className={style.modal} ref={dialogRef}>
+            <button className={style.close} onClick={closeDialog}>
+                <span className="hidden-text">close</span>
+                <img className={style.icon} src={xmarkSvg} alt="close icon" />
+            </button>
             {children}
         </dialog>
     )
