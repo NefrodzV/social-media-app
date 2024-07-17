1d6eb6855047ac5eacbd848616bbfc64d8fb9266
Neftaly <neftalyrv23@gmail.com>
2024-07-17 14:55:06 -0400

    made toast send with types

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,14 +1,22 @@
 import { useNotification } from '../hooks'
-export default function Toast({ id, message, type }) {
+import infoIcon from '../assets/svgs/info-solid.svg'
+import errorIcon from '../assets/svgs/exclamation-solid.svg'
+import successIcon from '../assets/svgs/check-solid.svg'
+export default function Toast({ id, message, type = 'info' }) {
+    const icons = {
+        info: infoIcon,
+        error: errorIcon,
+        success: successIcon,
+    }
     const { removeToast } = useNotification()
 
     return (
         <div className={`toast bottom`} role="alert">
+            <img src={icons[type]} alt="alert icon" />
+            <div>{message}</div>
             <div>
                 <button onClick={removeToast.bind('id', id)}>X</button>
             </div>
-
-            <div>{message}</div>
         </div>
     )
 }
