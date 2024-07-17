39bef28a3964efeefa920f8ed38a294fc84635fa
Neftaly <neftalyrv23@gmail.com>
2024-07-17 16:25:37 -0400

    Moved the styles for toast to a module and styles the toasts. Changed the toast icons and added  tweo new ones

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -2,6 +2,8 @@ import { useNotification } from '../hooks'
 import infoIcon from '../assets/svgs/info-solid.svg'
 import errorIcon from '../assets/svgs/exclamation-solid.svg'
 import successIcon from '../assets/svgs/check-solid.svg'
+import style from '../stylesheets/Toast.module.css'
+import closeIcon from '../assets/svgs/xmark-solid.svg'
 export default function Toast({ id, message, type = 'info' }) {
     const icons = {
         info: infoIcon,
@@ -11,12 +13,14 @@ export default function Toast({ id, message, type = 'info' }) {
     const { removeToast } = useNotification()
 
     return (
-        <div className={`toast bottom`} role="alert">
-            <img src={icons[type]} alt="alert icon" />
-            <div>{message}</div>
-            <div>
-                <button onClick={removeToast.bind('id', id)}>X</button>
-            </div>
+        <div className={`${style.toast}`} role="alert">
+            <img className={style.icon} src={icons[type]} alt="alert icon" />
+            <p className={style.message}>{message}</p>
+
+            <button onClick={removeToast.bind('id', id)}>
+                <img src={closeIcon} />
+                <span className="hidden-text">Close message</span>
+            </button>
         </div>
     )
 }
