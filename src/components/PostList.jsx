175b602da1f58dc5e0824dcb4ae57fff270bd8a9
Neftaly <neftalyrv23@gmail.com>
2024-07-25 10:11:41 -0400

    Added an error text if posts is empty

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
 import style from '../stylesheets/PostList.module.css'
-export default function PostList({ children, classNames }) {
+export default function PostList({ children, classNames, emptyListError }) {
     return (
         <section className={style.list + ` ${classNames || ''}`}>
             <h1 className={style.title}>Posts</h1>
-            <div className={style.container}>{children}</div>
+            {children?.length !== 0 ? (
+                <div className={style.container}>{children}</div>
+            ) : (
+                <div className="container-center-items">{emptyListError}</div>
+            )}
         </section>
     )
 }
