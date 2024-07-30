19a6b3689bcbd3a60afc235371b826ee19ed858b
Neftaly <neftalyrv23@gmail.com>
2024-07-30 15:22:11 -0400

    removed commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
 import Router from './Router'
 import { AuthProvider, NotificationProvider, DialogProvider } from './providers'
 function App() {
-    //     useEffect(() => {
-    //         async function getUser() {
-    //             try {
-    //                 setStatus(STATUS.PENDING)
-    //                 const response = await fetch(
-    //                     `${import.meta.env.VITE_API_URL}/users/me`,
-    //                     {
-    //                         mode: 'cors',
-    //                         credentials: 'include',
-    //                     }
-    //                 )
-    //                 const json = await response.json()
-    //                 if (!response.ok) {
-    //                     setStatus(STATUS.ERROR)
-    //                     return
-    //                 }
-    //                 setStatus(STATUS.SUCCESS)
-    //                 setUser(json.user)
-    //             } catch (e) {
-    //                 setStatus(STATUS.ERROR)
-    //                 set('isAuthenticated', false)
-    //                 throw new Error('GET auth user error ' + e)
-    //             }
-    //         }
-    //
-    //         if (isAuthenticated) getUser()
-    //         if (!isAuthenticated) setUser(null)
-    //     }, [isAuthenticated])
-
     return (
         <AuthProvider>
             <NotificationProvider>
